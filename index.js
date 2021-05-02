1f9614c4c397759c7cf71a9baac5cdf35a92ba82
Alberto <ajsaenz1091@gmail.com>
2021-05-02 14:22:47 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+//* First function declaration to pass as callback function to reduce
+// function reducer(accu, item){
+//     return accu += item;
+// }
+
+//* Second function declaration to pass as callback function to reduce
+const reducer = (acc,item) => {
+    return acc += item;
+}
+
+//* First implementation of reduce with built-in callback function
+
+// const totalBatteries = batteryBatches.reduce((accu, item) => {
+//     return accu += item;
+// }, 0);
+
+//* Second implementation of reduce passing built-out callback function
+
+ const totalBatteries = batteryBatches.reduce(reducer, 0);
